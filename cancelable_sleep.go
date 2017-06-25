2cea9adfcb60f448e2729ce3313d2a888c423638
zgs225 <zgs225@gmail.com>
2017-06-25 22:40:19 +0800

    fix

--- cancelable_sleep.go
+++ cancelable_sleep.go
@@ -1,20 +1,28 @@
 package maatq
 
 import (
+	"sync/atomic"
 	"time"
 )
 
 type cancelSleep struct {
 	quit chan int
+	flag int32
 }
 
 func (cs *cancelSleep) Sleep(d time.Duration) {
-	go func() { time.AfterFunc(d, cs.Cancel) }()
-	<-cs.quit
+	if atomic.LoadInt32(&cs.flag) == 0 {
+		atomic.StoreInt32(&cs.flag, 1)
+		go func() { time.AfterFunc(d, cs.Cancel) }()
+		<-cs.quit
+	}
 }
 
 func (cs *cancelSleep) Cancel() {
-	cs.quit <- 1
+	if atomic.LoadInt32(&cs.flag) == 1 {
+		cs.quit <- 1
+		atomic.StoreInt32(&cs.flag, 0)
+	}
 }
 
 func newCancelSleep() *cancelSleep {
--- cancelable_sleep_test.go
+++ cancelable_sleep_test.go
@@ -21,3 +21,29 @@ func TestCancelSleep(t *testing.T) {
 	}
 	t.Log("Sleep", time.Since(be))
 }
+
+func TestCancelBeforeSleep(t *testing.T) {
+	cs := newCancelSleep()
+	ch := make(chan bool)
+	cs.Cancel()
+	be := time.Now()
+	go func() {
+		cs.Sleep(time.Second)
+		ch <- true
+	}()
+	<-ch
+	if time.Since(be) < time.Second {
+		t.Error("Cancel before sleep error")
+	}
+	t.Log("Sleep", time.Since(be))
+}
+
+func BenchmarkCancelSleep(b *testing.B) {
+	s := newCancelSleep()
+	for i := 0; i < b.N; i++ {
+		go func() {
+			s.Sleep(time.Second)
+		}()
+		s.Cancel()
+	}
+}
